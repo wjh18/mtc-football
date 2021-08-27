6c63d80243af7fd28c21afbaa6ff7d30e1afe486
Will Holmes <willholmes95@gmail.com>
2021-08-26 22:15:16 -0400

    Add another base template for leagues with simulate dropdown. Add contact page, url and view

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, ContactPageView
 
 urlpatterns = [
+    path('contact/', ContactPageView.as_view(), name='contact'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('', HomePageView.as_view(), name='home'),
 ]
