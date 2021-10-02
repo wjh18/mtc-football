dbd10d37f9f9813f661b0893596aa90f579494b1
Will Holmes <willholmes95@gmail.com>
2021-10-02 17:11:49 -0400

    Update home/away standings in TeamStanding instances after each week

--- leagues/utils/update_standings.py
+++ leagues/utils/update_standings.py
@@ -39,42 +39,26 @@ def update_standings(season, current_week, matchups):
         scores = matchup.scoreboard.get_score()
         winner = matchup.scoreboard.get_winner()
         matchup_type = get_matchup_type(matchup)
-
+        
         for team in (matchup.home_team, matchup.away_team):
             current_standing = TeamStanding.objects.get(
                 team=team, season=season,
-                week_number=current_week)
-
-            # Update regular standings
+                week_number=current_week)       
+            # Get current regular standings data        
             wins = current_standing.wins
             losses = current_standing.losses
             ties = current_standing.ties
             streak = current_standing.streak
-
-            if winner == 'Tie':
-                ties += 1
-                streak = 0
-            elif winner == team:
-                wins += 1
-                streak += 1
-            else:
-                losses += 1
-                streak = 0
-
-            # Update PF and PA
-            if team == matchup.home_team:
-                points_for = current_standing.points_for + scores['Home']
-                points_against = current_standing.points_against + \
-                    scores['Away']
-            else:
-                points_for = current_standing.points_for + scores['Away']
-                points_against = current_standing.points_against + \
-                    scores['Home']
-            
-            # Update Home/Away standings
-                    
-            # Update type standings
-            
+            points_for = current_standing.points_for
+            points_against = current_standing.points_against
+            # Get current home/away standings data
+            home_wins = current_standing.home_wins
+            home_losses = current_standing.home_losses
+            home_ties = current_standing.home_ties
+            away_wins = current_standing.away_wins
+            away_losses = current_standing.away_losses
+            away_ties = current_standing.away_ties
+            # Get current type standings data
             div_wins = current_standing.div_wins
             div_losses = current_standing.div_losses
             div_ties = current_standing.div_ties
@@ -84,7 +68,40 @@ def update_standings(season, current_week, matchups):
             non_conf_wins = current_standing.non_conf_wins
             non_conf_losses = current_standing.non_conf_losses
             non_conf_ties = current_standing.non_conf_ties
-            
+
+            # Update W/L/T, PF/PA, and Home/Away standings
+            if team == matchup.home_team:
+                points_for += scores['Home']
+                points_against += scores['Away']
+                if winner == 'Tie':
+                    ties += 1
+                    home_ties += 1
+                    streak = 0
+                elif winner == team:
+                    wins += 1
+                    home_wins += 1
+                    streak += 1
+                else:
+                    losses += 1
+                    home_losses += 1
+                    streak = 0
+            else:
+                points_for += scores['Away']
+                points_against += scores['Home']
+                if winner == 'Tie':
+                    ties += 1
+                    away_ties += 1
+                    streak = 0
+                elif winner == team:
+                    wins += 1
+                    away_wins += 1
+                    streak += 1
+                else:
+                    losses += 1
+                    away_losses += 1
+                    streak = 0
+                    
+            # Update type standings    
             if matchup_type == 'Divisional':
                 if winner == 'Tie':
                     div_ties += 1                    
@@ -114,10 +131,13 @@ def update_standings(season, current_week, matchups):
                 team=team, season=season, week_number=current_week + 1,
                 wins=wins, losses=losses, ties=ties, streak=streak,
                 points_for=points_for, points_against=points_against,
+                home_wins=home_wins, home_losses=home_losses, home_ties=home_ties,
+                away_wins=away_wins, away_losses=away_losses, away_ties=away_ties,
                 div_wins=div_wins, div_losses=div_losses, div_ties=div_ties,
                 conf_wins=conf_wins, conf_losses=conf_losses, conf_ties=conf_ties,
                 non_conf_wins=non_conf_wins, non_conf_losses=non_conf_losses,
                 non_conf_ties=non_conf_ties)
+                # last_5_wins=last_5_wins, last_5_losses=last_5_losses, last_5_ties=last_5_ties,
 
 def generate_division_rankings(season):
     """
