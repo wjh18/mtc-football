0a77edb38097b88cbef9e38e606e3085696bf4a0
Will Holmes <willholmes95@gmail.com>
2022-09-26 15:44:10 -0400

    Allow disabling of debug toolbar in development mode, update the INTERNAL_IPs it sets for the Docker container, and disable all debug panels by default

--- config/settings.py
+++ config/settings.py
@@ -152,7 +152,28 @@ MESSAGE_TAGS = {
 }
 
 # django-debug-toolbar
-import socket
-
-hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
+if DEBUG:
+    import socket  # only if you haven't already imported this
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
+DEBUG_TOOLBAR_CONFIG = {
+    'DISABLE_PANELS': {
+        'debug_toolbar.panels.history.HistoryPanel',
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel',
+    },
+    'SHOW_COLLAPSED': True,
+    'SHOW_TOOLBAR_CALLBACK': 'apps.utils.config.show_toolbar'
+}
+SHOW_TOOLBAR = True # Disable toolbar globally
