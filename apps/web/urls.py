84aca3d037da242c6cb819a24e159fe8dde88985
Will Holmes <willholmes95@gmail.com>
2022-09-26 16:32:44 -0400

    Change location of error templates from base to errors

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('contact/', ContactPageView.as_view(), name='contact'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('', HomePageView.as_view(), name='home'),
-    path('404/', TemplateView.as_view(template_name='base/404.html'), name='404'),
-    path('500/', TemplateView.as_view(template_name='base/500.html'), name='500'),
-    path('403/', TemplateView.as_view(template_name='base/403.html'), name='403'),
-    path('400/', TemplateView.as_view(template_name='base/400.html'), name='400'),
+    path('404/', TemplateView.as_view(template_name='errors/404.html'), name='404'),
+    path('500/', TemplateView.as_view(template_name='errors/500.html'), name='500'),
+    path('403/', TemplateView.as_view(template_name='errors/403.html'), name='403'),
+    path('400/', TemplateView.as_view(template_name='errors/400.html'), name='400'),
 ]
