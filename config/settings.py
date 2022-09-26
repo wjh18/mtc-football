127465971e05424d1b9aa02cfbb770b80b46f796
Will Holmes <willholmes95@gmail.com>
2022-09-26 07:38:55 -0400

    Remove explicit i18n settings (default to True now in Django 4+)

--- config/settings.py
+++ config/settings.py
@@ -102,8 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'America/New_York'
 USE_I18N = True
-USE_L10N = True
-USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
