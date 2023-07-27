f65a5dcdc29cccc2ff4b1c77489e8f1e3f33526e
Will Holmes <willholmes95@gmail.com>
2023-07-27 09:14:08 -0400

    Modify error template paths in web URL conf based on previous change

--- apps/web/urls.py
+++ apps/web/urls.py
@@ -6,10 +6,10 @@ from . import views
 app_name = 'web'
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('404/', TemplateView.as_view(template_name='errors/404.html'), name='404'),
-    path('500/', TemplateView.as_view(template_name='errors/500.html'), name='500'),
-    path('403/', TemplateView.as_view(template_name='errors/403.html'), name='403'),
-    path('400/', TemplateView.as_view(template_name='errors/400.html'), name='400'),
+    path('404/', TemplateView.as_view(template_name='404.html'), name='404'),
+    path('500/', TemplateView.as_view(template_name='500.html'), name='500'),
+    path('403/', TemplateView.as_view(template_name='403.html'), name='403'),
+    path('400/', TemplateView.as_view(template_name='400.html'), name='400'),
     path('about/', views.AboutPageView.as_view(), name='about'),
     path('contact/', views.contact_view, name='contact'),
     path('terms/', views.TermsPageView.as_view(), name='terms'),
